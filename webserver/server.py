41e4c451698a92847c64a01a699b8e4cfb645b98
Mingfeng Li_XPS13 <keithlimingfeng@gmail.com>
2019-04-16 01:32:44 -0400

    update

--- webserver/server.py
+++ webserver/server.py
@@ -25,7 +25,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 
 
 
-# XXX: The Database URI should be in the format of: 
+# XXX: The Database URI should be in the format of:
 #
 #     postgresql://USER:PASSWORD@<IP_OF_POSTGRE_SQL_SERVER>/<DB_NAME>
 #
@@ -36,8 +36,8 @@ app = Flask(__name__, template_folder=tmpl_dir)
 # For your convenience, we already set it to the class database
 
 # Use the DB credentials you received by e-mail
-DB_USER = "YOUR_DB_USERNAME_HERE"
-DB_PASSWORD = "YOUR_DB_PASSWORD_HERE"
+DB_USER = "st3185"
+DB_PASSWORD = "xo9AGrNiHw"
 
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
 
@@ -63,7 +63,7 @@ engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'
 @app.before_request
 def before_request():
   """
-  This function is run at the beginning of every web request 
+  This function is run at the beginning of every web request
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request
 
@@ -97,7 +97,7 @@ def teardown_request(exception):
 #       @app.route("/foobar/", methods=["POST", "GET"])
 #
 # PROTIP: (the trailing / in the path is important)
-# 
+#
 # see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
 # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 #
@@ -120,10 +120,10 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
+  cursor = g.conn.execute("SELECT p_id FROM building JOIN property ON building.bldg_id = property.bldg_id WHERE year_built >2000")
+  p_id = []
   for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
+    p_id.append(result['p_id'])  # can also be accessed using result[0]
   cursor.close()
 
   #
@@ -135,14 +135,14 @@ def index():
   # You can see an example template in templates/index.html
   #
   # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
+  # for example, "data" key in the context variable defined below will be
   # accessible as a variable in index.html:
   #
   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
   #     <div>{{data}}</div>
-  #     
+  #
   #     # creates a <div> tag for each element in data
-  #     # will print: 
+  #     # will print:
   #     #
   #     #   <div>grace hopper</div>
   #     #   <div>alan turing</div>
@@ -152,42 +152,361 @@ def index():
   #     <div>{{n}}</div>
   #     {% endfor %}
   #
-  context = dict(data = names)
+  context = dict(data = p_id)
 
 
   #
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html", **context)
+  return render_template("homepage.html", **context)
 
 #
 # This is an example of a different path.  You can see it at
-# 
+#
 #     localhost:8111/another
 #
 # notice that the functio name is another() rather than index()
 # the functions for each app.route needs to have different names
 #
-@app.route('/another')
-def another():
-  return render_template("anotherfile.html")
+@app.route('/to_addBuyer')
+def to_addBuyer():
+  return render_template("addBuyer.html")
+
+@app.route('/to_addSeller')
+def to_addSeller():
+   return render_template("addSeller.html")
+
+@app.route('/to_login')
+def to_login():
+   return render_template("Login.html")
+
+@app.route('/homepage')
+def homepage():
+  return render_template("homepage.html")
+
+@app.route('/to_buyerprofile')
+def to_buyerprofile():
+    cursor = g.conn.execute("SELECT property.p_id,prop_type, lot_size, bathroom_n, bedroom_n, furnished, garage, address, city, state, building.zipcode, neighborhood, year_built, description FROM property JOIN building ON property.bldg_id = building.bldg_id JOIN Zipcodes ON building.zipcode = Zipcodes.zipcode")
+    info = []
+    for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['prop_type'] = result[1]
+        dic['lot_size'] = result[2]
+        dic['bathroom_n'] = result[3]
+        dic['bedroom_n'] = result[4]
+        dic['furnished'] = result[5]
+        dic['garage'] = result[6]
+        dic['address'] = result[7]
+        dic['city'] = result[8]
+        dic['state'] = result[9]
+        dic['zipcode'] = result[10]
+        dic['neighborhood'] = result[11]
+        dic['year_built'] = result[12]
+        dic['description'] = result[13]
+        info.append(dic)
+    cursor.close()
+    cursor = g.conn.execute("SELECT p_id FROM likes WHERE b_id = 1")
+    info2 = []
+    for result in cursor:
+      dic = dict()
+      dic['p_id'] = result[0]
+      info2.append(dic)
+    cursor.close()
+    context = dict(Property = info, Liked = info2)
+    return render_template("buyerprofile.html", **context)
 
 
 # Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  print name
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
-  return redirect('/')
+
+@app.route('/addBuyer', methods=['POST'])
+def addBuyer():
+  b_username = request.form['b_username']
+  b_password = request.form['b_password']
+  b_email = request.form['b_email']
+  #print name
+  #users_id need to be different from each other
+  result1 = g.conn.execute(text('SELECT max(b_id) from buyer'))
+  b_id=0
+  for i in result1:
+      b_id =i[0]+1
+  result = g.conn.execute(text('SELECT b_username from buyer'))
+  buyer_username_list = []
+  for r in result:
+      buyer_username_list.append(str(r[0]))
+  if b_username in buyer_username_list:
+      return render_template('useriderror.html')
+  else:
+      cmd = 'INSERT INTO buyer(b_id,b_username,b_password,b_email) VALUES (:b_id,:b_username,:b_password,:b_email)';
+      g.conn.execute(text(cmd), b_id = b_id, b_username = b_username,  b_password =b_password,b_email =b_email);
+      return render_template("successfulcreate.html")
+
+@app.route('/addSeller', methods=['POST'])
+def addSeller():
+  s_username = request.form['s_username']
+  s_password = request.form['s_password']
+  s_email = request.form['s_email']
+  phone_number = request.form['phone_number']
+  #print name
+  #users_id need to be different from each other
+  result = g.conn.execute(text('SELECT max(s_id) from seller'))
+  s_id=0
+  for i in result:
+      s_id =i[0]+1
+  result1 = g.conn.execute(text('SELECT s_username from seller'))
+  seller_username_list = []
+  for r in result1:
+      seller_username_list.append(str(r[0]))
+  if s_username in seller_username_list:
+      return render_template('useriderror.html')
+  else:
+      cmd = 'INSERT INTO seller(s_id,s_username,s_password,s_email,phone_number) VALUES (:s_id,:s_username,:s_password,:s_email,:phone_number)';
+      g.conn.execute(text(cmd), s_id = s_id, s_username = s_username,  s_password =s_password,s_email =s_email, phone_number =phone_number);
+      return render_template("successfulcreate.html")
+
+@app.route('/Login', methods=['POST'])
+def Login():
+  username = request.form['username']
+  password = request.form['password']
+  value = request.form['identity']
+  if username =="" or password =="":
+      return render_template("loginerror.html")
+  #print name
+  #users_id need to be different from each other
+  if value == "Buyer":
+      result1 = g.conn.execute(text('SELECT b_username from buyer'))
+      buyer_username_list = []
+      for r in result1:
+          buyer_username_list.append(str(r[0]))
+      result2 = g.conn.execute(text('SELECT b_password from buyer'))
+      buyer_password_list = []
+      for r in result2:
+          buyer_password_list.append(str(r[0]))
+      if username not in buyer_username_list or password not in buyer_password_list:
+          return render_template('loginerror.html')
+      if username =="b1" and password !="b1":
+          return render_template('loginerror.html')
+      cursor = g.conn.execute("SELECT property.p_id,prop_type, lot_size, bathroom_n, bedroom_n, furnished, garage, address, city, state, building.zipcode, neighborhood, year_built, description FROM property JOIN building ON property.bldg_id = building.bldg_id JOIN Zipcodes ON building.zipcode = Zipcodes.zipcode")
+      info = []
+      for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['prop_type'] = result[1]
+        dic['lot_size'] = result[2]
+        dic['bathroom_n'] = result[3]
+        dic['bedroom_n'] = result[4]
+        dic['furnished'] = result[5]
+        dic['garage'] = result[6]
+        dic['address'] = result[7]
+        dic['city'] = result[8]
+        dic['state'] = result[9]
+        dic['zipcode'] = result[10]
+        dic['neighborhood'] = result[11]
+        dic['year_built'] = result[12]
+        dic['description'] = result[13]
+        info.append(dic)
+      cursor.close()
+      context = dict(Property = info)
+      cursor = g.conn.execute("SELECT p_id FROM likes WHERE b_id = 1")
+      info2 = []
+      for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        info2.append(dic)
+      cursor.close()
+      context = dict(Property = info, Liked = info2)
+      return render_template('buyerprofile.html', **context)
+  else:
+      result1 = g.conn.execute(text('SELECT s_username from seller'))
+      seller_username_list = []
+      for r in result1:
+          seller_username_list.append(str(r[0]))
+      result2 = g.conn.execute(text('SELECT s_password from seller'))
+      seller_password_list = []
+      for r in result2:
+          seller_password_list.append(str(r[0]))
+      if username not in seller_username_list or password not in seller_password_list:
+          return render_template('loginerror.html')
+      if username =="s5" and password !="s5":
+          return render_template('loginerror.html')
+      cursor = g.conn.execute("SELECT property.p_id, prop_type, lot_size, bathroom_n, bedroom_n, furnished, garage, address, city, state, building.zipcode, neighborhood, year_built, description FROM property JOIN building ON property.bldg_id = building.bldg_id JOIN Zipcodes ON building.zipcode = Zipcodes.zipcode JOIN listing ON listing.p_id = property.p_id WHERE s_id = 5")
+      info = []
+      for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['prop_type'] = result[1]
+        dic['lot_size'] = result[2]
+        dic['bathroom_n'] = result[3]
+        dic['bedroom_n'] = result[4]
+        dic['furnished'] = result[5]
+        dic['garage'] = result[6]
+        dic['address'] = result[7]
+        dic['city'] = result[8]
+        dic['state'] = result[9]
+        dic['zipcode'] = result[10]
+        dic['neighborhood'] = result[11]
+        dic['year_built'] = result[12]
+        dic['description'] = result[13]
+        info.append(dic)
+      cursor.close()
+      cursor = g.conn.execute("SELECT  p_id, b_username, b_email FROM likes JOIN buyer ON likes.b_id = buyer.b_id WHERE p_id IN (SELECT p_id FROM listing WHERE s_id = 5) ORDER BY p_id")
+      info2 = []
+      for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['b_username'] = result[1]
+        dic['b_email'] = result[2]
+        info2.append(dic)
+      cursor.close()
+      context = dict(Property = info, Liked = info2)
+      return render_template('sellerprofile.html', **context)
+
+@app.route('/productList',methods=['GET'])
+def productList():
+
+  return render_template("productList.html", **context)
+PRO_ID=['a','b']
+
+# delete data from the databases
+@app.route('/Delete', methods=['POST'])
+def Delete():
+    p_id = request.form['p_id']
+    cmd = 'DELETE FROM property WHERE p_id = :p_id'
+    g.conn.execute(text(cmd),p_id=p_id)
+    cursor = g.conn.execute("SELECT property.p_id, prop_type, lot_size, bathroom_n, bedroom_n, furnished, garage, address, city, state, building.zipcode, neighborhood, year_built, description FROM property JOIN building ON property.bldg_id = building.bldg_id JOIN Zipcodes ON building.zipcode = Zipcodes.zipcode JOIN listing ON listing.p_id = property.p_id WHERE s_id = 5")
+    info = []
+    for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['prop_type'] = result[1]
+        dic['lot_size'] = result[2]
+        dic['bathroom_n'] = result[3]
+        dic['bedroom_n'] = result[4]
+        dic['furnished'] = result[5]
+        dic['garage'] = result[6]
+        dic['address'] = result[7]
+        dic['city'] = result[8]
+        dic['state'] = result[9]
+        dic['zipcode'] = result[10]
+        dic['neighborhood'] = result[11]
+        dic['year_built'] = result[12]
+        dic['description'] = result[13]
+        info.append(dic)
+    cursor.close()
+    cursor = g.conn.execute("SELECT  p_id, b_username, b_email FROM likes JOIN buyer ON likes.b_id = buyer.b_id WHERE p_id IN (SELECT p_id FROM listing WHERE s_id = 5) ORDER BY p_id")
+    info2 = []
+    for result in cursor:
+      dic = dict()
+      dic['p_id'] = result[0]
+      dic['b_username'] = result[1]
+      dic['b_email'] = result[2]
+      info2.append(dic)
+    cursor.close()
+    context = dict(Property = info, Liked = info2)
+    return render_template("sellerprofile.html", **context)
 
 
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
+@app.route('/Like', methods=['POST'])
+def Like():
+    p_id = request.form['p_id']
+    result = g.conn.execute(text('SELECT p_id from property'))
+    p_id_list = []
+    for r in result:
+        p_id_list.append(str(r[0]))
+    if p_id not in p_id_list:
+        return render_template('piderror.html')
+    result2 = g.conn.execute("SELECT p_id FROM likes WHERE b_id = 1")
+    p_id_list2 = []
+    for result in result2:
+        p_id_list2.append(str(result[0]))
+    if p_id not in p_id_list2:
+        cmd = 'INSERT INTO likes(b_id, p_id) VALUES (1,:p_id)'
+        g.conn.execute(text(cmd),p_id=p_id)
+    cursor = g.conn.execute("SELECT property.p_id,prop_type, lot_size, bathroom_n, bedroom_n, furnished, garage, address, city, state, building.zipcode, neighborhood, year_built, description FROM property JOIN building ON property.bldg_id = building.bldg_id JOIN Zipcodes ON building.zipcode = Zipcodes.zipcode")
+    info = []
+    for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['prop_type'] = result[1]
+        dic['lot_size'] = result[2]
+        dic['bathroom_n'] = result[3]
+        dic['bedroom_n'] = result[4]
+        dic['furnished'] = result[5]
+        dic['garage'] = result[6]
+        dic['address'] = result[7]
+        dic['city'] = result[8]
+        dic['state'] = result[9]
+        dic['zipcode'] = result[10]
+        dic['neighborhood'] = result[11]
+        dic['year_built'] = result[12]
+        dic['description'] = result[13]
+        info.append(dic)
+    cursor.close()
+    cursor = g.conn.execute("SELECT p_id FROM likes WHERE b_id = 1")
+    info2 = []
+    for result in cursor:
+      dic = dict()
+      dic['p_id'] = result[0]
+      info2.append(dic)
+    cursor.close()
+    context = dict(Property = info, Liked = info2)
+    return render_template("buyerprofile.html", **context)
+
+
+@app.route('/Add_P', methods=['POST'])
+def Add_P():
+    result = g.conn.execute(text('SELECT max(p_id) from property'))
+    p_id=0
+    for i in result:
+        p_id =i[0]+1
+    prop_type = request.form['prop_type']
+    lot_size = request.form['lot_size']
+    bathroom_n = request.form['bathroom_n']
+    bedroom_n = request.form['bedroom_n']
+    furnished = request.form['furnished']
+    garage = request.form['garage']
+    bldg_id = request.form['bldg_id']
+    s_id =5
+    cmd = 'INSERT INTO property(p_id,prop_type,bldg_id,lot_size,bathroom_n,bedroom_n, furnished, garage) \
+    VALUES(:p_id,:prop_type,:bldg_id,:lot_size,:bathroom_n,:bedroom_n,:furnished,:garage)'
+    g.conn.execute(text(cmd),p_id=p_id,prop_type =prop_type,bldg_id = bldg_id,lot_size=lot_size,bathroom_n=bathroom_n,bedroom_n =bedroom_n,furnished=furnished,garage=garage)
+    cmd = 'INSERT INTO listing(s_id,p_id) VALUES(:s_id,:p_id)'
+    g.conn.execute(text(cmd),s_id=s_id,p_id=p_id)
+
+
+    cursor = g.conn.execute("SELECT property.p_id, prop_type, lot_size, bathroom_n, bedroom_n, furnished, garage, address, city, state, building.zipcode, neighborhood, year_built, description FROM property JOIN building ON property.bldg_id = building.bldg_id JOIN Zipcodes ON building.zipcode = Zipcodes.zipcode JOIN listing ON listing.p_id = property.p_id WHERE s_id = 5")
+    info = []
+    for result in cursor:
+        dic = dict()
+        dic['p_id'] = result[0]
+        dic['prop_type'] = result[1]
+        dic['lot_size'] = result[2]
+        dic['bathroom_n'] = result[3]
+        dic['bedroom_n'] = result[4]
+        dic['furnished'] = result[5]
+        dic['garage'] = result[6]
+        dic['address'] = result[7]
+        dic['city'] = result[8]
+        dic['state'] = result[9]
+        dic['zipcode'] = result[10]
+        dic['neighborhood'] = result[11]
+        dic['year_built'] = result[12]
+        dic['description'] = result[13]
+        info.append(dic)
+    cursor.close()
+    cursor = g.conn.execute("SELECT  p_id, b_username, b_email FROM likes JOIN buyer ON likes.b_id = buyer.b_id WHERE p_id IN (SELECT p_id FROM listing WHERE s_id = 5) ORDER BY p_id")
+    info2 = []
+    for result in cursor:
+      dic = dict()
+      dic['p_id'] = result[0]
+      dic['b_username'] = result[1]
+      dic['b_email'] = result[2]
+      info2.append(dic)
+    cursor.close()
+    context = dict(Property = info, Liked = info2)
+    return render_template("sellerprofile.html", **context)
+
 
 
 if __name__ == "__main__":
